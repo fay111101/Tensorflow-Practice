a4a1873d080f926f7bba604ab5622821c73ea8de
fay111101 <1139598278@qq.com>
2018-07-09 17:58:17 +0800

    rnn

--- mnist/mnist_train.py
+++ mnist/mnist_train.py
@@ -12,66 +12,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 import mnist_inference
-#
-# BATCH_SIZE = 100
-# # 模型相关的参数
-# LEARNING_RATE_BASE = 0.8  # 基础学习率
-# LEARNING_RATE_DECAY = 0.99  # 学习率的衰减率
-# REGULARAZTION_RATE = 0.0001  # 描述模型复杂度的正则化项在损失函数中的系数
-# # TRAINING_STEPS = 5000  # 训练轮数
-# TRAINING_STEPS = 30000  # 训练轮数
-# MOVING_AVERAGE_DECAY = 0.99  # 滑动平均衰减率
-#
-# # 模型保存的路径和文件名
-# MODEL_SAVE_PATH = './model/'
-# MODEL_NAME = 'model.ckpt'
-#
-#
-# def train(mnist):
-#     x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x-input')
-#     y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-output')
-#
-#     regualizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE)
-#     y = mnist_inference.inference1(x, regualizer)
-#     global_step = tf.Variable(0, trainable=False)
-#     variable_average = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
-#     variable_average_op = variable_average.apply(tf.trainable_variables())
-#     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.argmax(y_, 1), logits=y)
-#     cross_entropy_mean = tf.reduce_mean(cross_entropy)
-#     loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
-#     learning_rate = tf.train.exponential_decay(
-#         LEARNING_RATE_BASE,
-#         global_step,
-#         mnist.train.num_examples / BATCH_SIZE,
-#         LEARNING_RATE_DECAY,
-#         staircase=True)
-#     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-#     with tf.control_dependencies([train_step, variable_average_op]):
-#         train_op = tf.no_op(name='train')
-#
-#     saver = tf.train.Saver()
-#     with tf.Session() as sess:
-#         tf.global_variables_initializer().run()
-#
-#         for i in range(TRAINING_STEPS):
-#             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-#             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={
-#                 x: xs, y_: ys
-#             })
-#
-#             if i % 1000 == 0:
-#                 print("After %d trainning steps,loss on trainning batch is %g" % (step, loss_value))
-#                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
-#
-#
-# def main(argv=None):
-#     # mnist = input_data.read_data_sets('MINIST_data', one_hot=True)
-#     mnist = read_data_sets('MINIST_data', one_hot=True)
-#     train(mnist)
-#
-#
-# if __name__ == '__main__':
-#     main()
+
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
 LEARNING_RATE_DECAY = 0.99
---
+++ rnn/predict_sin.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 18-7-9 下午3:05
+@Author  : fay
+@Email   : fay625@sina.cn
+@File    : predict_sin.py TF1.8
+@Software: PyCharm
+"""
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from tensorflow.contrib import rnn
+
+'''
+参考
+https://blog.csdn.net/yunge812/article/details/79444089
+'''
+HIDDEN_SIZE = 30  # LSTM中隐藏节点的数目
+NUM_LAYERS = 2  # LSTM的层数
+TIMESTEPS = 10  # 训练序列长度
+TRAINING_STEPS = 10000  # 训练轮数
+BATCH_SIZE = 32  # batch大小
+TRAIN_EXAMPLES = 10000  # 训练数据个数
+TEST_EXAMPLES = 1000
+SAMPLE_GAP = 0.01  # 采样间隔
+
+
+def generate_data(seq):
+    X = []
+    Y = []
+    # 序列的第i项和后面的TIMESTEPS-1项合在一起作为输入;第i+TIMESTEPS项作为输出
+    # 即用sin函数前面的TIMESTPES个点的信息，预测第i+TIMESTEPS个点的函数值
+    for i in range(len(seq) - TIMESTEPS - 1):
+        X.append([seq[i:i + TIMESTEPS]])
+        Y.append([seq[i + TIMESTEPS]])
+    return np.array(X, dtype=np.float32), np.array(Y, dtype=np.float32)
+
+
+def LstmCell():
+    lstm_cell = rnn.BasicLSTMCell(HIDDEN_SIZE, state_is_tuple=True)
+    return lstm_cell
+
+
+# 定义lstm模型
+def lstm_model(X, y):
+    cell = rnn.MultiRNNCell([LstmCell() for _ in range(NUM_LAYERS)])
+    output, _ = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+    output = tf.reshape(output, [-1, HIDDEN_SIZE])
+    # 通过无激活函数的全连接层计算线性回归，并将数据压缩成一维数组结构
+    predictions = tf.contrib.layers.fully_connected(output, 1, None)
+
+    # 将predictions和labels调整统一的shape
+    labels = tf.reshape(y, [-1])
+    predictions = tf.reshape(predictions, [-1])
+
+    loss = tf.losses.mean_squared_error(predictions, labels)
+    # train_op = tf.contrib.layers.optimize_loss(loss, tf.train.get_global_step(),
+    #                                            optimizer="Adagrad",
+    #                                            learning_rate=0.1)
+    train_op = tf.contrib.layers.optimize_loss(
+        loss,
+        tf.contrib.framework.get_global_step(),
+        optimizer="Adagrad",
+        learning_rate=0.1)
+    return predictions, loss, train_op
+
+
+def run_eval(sess, test_X, test_y):
+    # 将测试数据以数据集的方式提供给计算图。
+    ds = tf.data.Dataset.from_tensor_slices((test_X, test_y))
+    ds = ds.batch(1)
+    X, y = ds.make_one_shot_iterator().get_next()
+
+    # 调用模型得到计算结果，
+    with tf.variable_scope('model', reuse=True):
+        prediction, _, _ = lstm_model(X, [0.0], False)
+
+    predictions = []
+    labels = []
+    for i in range(TEST_EXAMPLES):
+        p, l = sess.run([prediction, y])
+        predictions.append(p)
+        labels.append(l)
+
+    print(predictions)
+    print(labels)
+    # 计算rmse
+    predictions = np.array(predictions).squeeze()
+    labels = np.array(labels).squeeze()
+    rmse = np.sqrt((predictions - labels) ** 2).mean(axis=0)
+    print("Root Mean Square Error is :%f", rmse)
+
+    plt.figure()
+    plt.plot(predictions, labels='predictions')
+    plt.plot(labels, label='real_sin')
+    plt.legend()
+    plt.show()
+
+
+def train(sess, train_X, train_y):
+    ds = tf.data.Dataset.from_tensor_slices(
+        {
+            'train_X': train_X,
+            'train_y': train_y
+        })
+    ds = ds.repeat().shuffle(buffer_size=1000).batch(batch_size=BATCH_SIZE)
+    X, y = ds.make_one_shot_iterator().get_next()
+
+    # 定义模型，得到预测结果，损失函数和训练操作
+    with tf.variable_scope('model'):
+        predictions, loss, train_op = lstm_model(X, y)
+
+    sess.run(tf.global_variables_initializer())
+    # 训练模型
+    for i in range(TRAINING_STEPS):
+        _, l = sess.run([train_op, loss])
+        if i % 100 == 0:
+            print('train step: ' + str(i) + ",loss:" + str(l))
+
+
+learn = tf.contrib.learn
+regressor = learn.Estimator(model_fn=lstm_model)
+test_start = TRAIN_EXAMPLES * SAMPLE_GAP
+test_end = (TRAIN_EXAMPLES + TEST_EXAMPLES) * SAMPLE_GAP
+# 产生仿真数据x  y 用于训练
+train_x, train_y = generate_data(np.sin(np.linspace(0, test_start,
+                                                    TRAIN_EXAMPLES, dtype=np.float32)))
+# 产生仿真数据x  y 用于测试
+test_x, test_y = generate_data(np.sin(np.linspace(test_start, test_end,
+                                                  TEST_EXAMPLES, dtype=np.float32)))
+
+regressor.fit(train_x, train_y, batch_size=BATCH_SIZE, steps=TRAINING_STEPS)
+predicted = [[pred] for pred in regressor.predict(test_x)]
+rmse = np.sqrt((predicted - test_y) ** 2).mean(axis=0)
+fig = plt.figure()
+plot_predicted = plt.plot(predicted, label="predicted", color='red')
+plot_test = plt.plot(test_y, label="real_sin", color='blue')
+plt.legend = ([plot_predicted, plot_test], ['predicted', 'real_sin'])
+plt.show()
+
+# test_start = TRAIN_EXAMPLES * SAMPLE_GAP
+# test_end = (TRAINEXAMPLES + TEST_EXAMPLES) * SAMPLE_GAP
+# train_X, train_y = generate_data(np.sin(np.linspace(0, test_start, TRAIN_EXAMPLES, dtype=np.float32)))
+# test_X, test_y = generate_data(np.sin(np.linspace(test_start, test_end, TEST_EXAMPLES, dtype=np.float32)))
+# with tf.Session() as sess:
+#     train(sess, train_X, train_y)
+#     run_eval(test_X, test_y)
+'''
+ValueError: Expected input tensor Tensor("model/rnn/Const:0", shape=(), dtype=string) to have rank at least 2
+The error seems fairly clear, tf.nn.dynamic_rnn expects a 3-dimensional tensor as input (i.e. rank 3), 
+but fc2 has only two dimensions. 
+The shape of fc2 should be something like (<batch_size>, <max_time>, <num_features>) (or (<max_time>, <batch_size>, 
+<num_features>) if you pass time_major=True)
+'''
